26e077408a174054e3df89a21fcab71338a86940
Austin-Burn <166449104+Austin-Burn@users.noreply.github.com>
2024-04-22 11:57:29 -0400

    first commit

---
+++ src/main/java/A/T/schedulebuilder/SchedulebuilderApplication.java
@@ -0,0 +1,13 @@
+package A.T.schedulebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulebuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulebuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/A/T/schedulebuilder/SchedulebuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package A.T.schedulebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulebuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
