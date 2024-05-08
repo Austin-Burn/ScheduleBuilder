a69bc59fa7dee5a8c284e3c4d8bfa009a5587944
victoria-sauder <vsauder@umd.edu>
2024-05-07 21:31:36 -0700

    Added logging

--- src/main/java/A/T/schedulebuilder/api/controller/EventController.java
+++ src/main/java/A/T/schedulebuilder/api/controller/EventController.java
@@ -3,6 +3,8 @@ package A.T.schedulebuilder.api.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,10 @@ import A.T.schedulebuilder.api.service.EventService;
 
 @RestController
 public class EventController {
-    private EventService eventService;
 
+    private static Logger log = LoggerFactory.getLogger(EventController.class);
+
+    private EventService eventService;
 
     @Autowired
     public EventController(EventService eventService){
@@ -37,8 +41,8 @@ public class EventController {
             }
             return list;
         } else {
+            log.info("Got events!");
             return eventService.getEvents();
         }
     }
-
 }
